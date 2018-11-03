0eaff79a73e5bb244c049f1264149c6ee246d3b5
Luigi Maestrelli <lgmaestrelli@gmail.com>
2018-11-03 18:34:37 -0300

    Install redux

--- poketimes/src/components/Home.js
+++ poketimes/src/components/Home.js
@@ -1,51 +1,53 @@
 import React, { Component } from 'react';
-import axios from 'axios';
+import { Link } from 'react-router-dom';
+import { connect }  from 'react-redux';
 
 class Home extends Component {
 
-    state = {
-        posts: []
-    };
-
-    componentDidMount() {
-        axios.get('https://jsonplaceholder.typicode.com/posts')
-            .then(response =>{
-                this.setState({
-                    posts: response.data.slice(0, 10)
-                });
-            });
-    }
-
     render() {
+        const { posts } = this.props;
 
-        const { posts } = this.state;
+        let postList;
 
-        const postList = posts.length ? (
-            posts.map(post => {
-                return (
-                    <div className="post card" key={post.id}>
-                        <div className="card-content">
-                            <span className="card-title">
-                                {post.title}
-                            </span>
-                            <p>{post.body}</p>
+        if (posts.length) {
+            postList = (
+                posts.map(post => {
+                    return (
+                        <div className="post card" key={post.id}>
+                            <div className="card-content">
+                                <Link to={'/' + post.id}>
+                                    <span className="card-title red-text">
+                                        {post.title}
+                                    </span>
+                                </Link>
+    
+                                <p>{post.body}</p>
+                            </div>
                         </div>
-                    </div>
-                )
-            })
-        ) : (
-            <div className="center">
-                No posts yet!
-            </div>
-        );
+                    )
+                })
+            );
+        } else {
+            postList = (
+                <div className="center">
+                    No posts yet!
+                </div>
+            );
+        }
 
         return (
-            <div className="container">
+            <div className="container home">
                 <h4 className="center">Home</h4>
                 {postList}
             </div>
-        );
-    };
-};
+        )
+    }
+}
 
-export default Home;
+const mapStateToProps = (state) => {
+    return {
+        posts: state.posts
+    }
+}
+
+export default connect(mapStateToProps)(Home);
